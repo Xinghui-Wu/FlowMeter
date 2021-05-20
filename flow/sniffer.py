8f609331eef680898031471c21416e0e9ddeed84
linxin98 <844340004@qq.com>
2021-05-20 21:15:47 +0800

    Add conversion from IP address to address.

--- flow/sniffer.py
+++ flow/sniffer.py
@@ -1,18 +1,9 @@
 from scapy.all import *
 from scapy.layers.inet import IP
 from django.utils import timezone
+import ipdb
 
 from flow.models import flow
-
-
-def save_flow(packet):
-    ip_packet = packet[IP]
-    flow_item = flow(src=ip_packet.src, dst=ip_packet.dst, time=timezone.now(), size=ip_packet.len, address='', app='')
-    flow_item.save()
-
-
-def initial_flow_table():
-    flow.objects.all().delete()
 
 
 class Sniffer:
@@ -20,13 +11,25 @@ class Sniffer:
         super(Sniffer, self).__init__()
         self.index = None
         self.device = None
+        self.db = ipdb.City('static/backend/ipipfree.ipdb')
+        # print(self.db.languages())
 
     def set_device_by_index(self, index):
         if self.index is None or self.index != index:
             self.index = index
             self.device = dev_from_index(index)
-            initial_flow_table()
+            self.initial_flow_table()
+
+    def save_flow(self, packet):
+        ip_packet = packet[IP]
+        flow_item = flow(src=ip_packet.src, dst=ip_packet.dst, time=timezone.now(), size=ip_packet.len, address='',
+                         app='')
+        print(self.db.find(ip_packet.dst, 'CN'))
+        flow_item.save()
+
+    def initial_flow_table(self):
+        flow.objects.all().delete()
 
     def sniff(self, timeout=1):
         print('start sniff')
-        sniff(count=0, prn=save_flow, filter='ip', timeout=timeout, iface=self.device)
+        sniff(count=0, prn=self.save_flow, filter='ip', timeout=timeout, iface=self.device)
