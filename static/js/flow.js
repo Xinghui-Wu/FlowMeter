0428987bd832ad09a81a4d646c0be7a861f0eef8
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-05-25 13:11:36 +0800

    Finish the basic functionalities.

--- static/js/flow.js
+++ static/js/flow.js
@@ -1,11 +1,18 @@
 /*
 * 页面铺满屏幕
 */
-function setFullScreen() {
+$(document).ready(function () {
     var screenHeight = document.documentElement.clientHeight;
     var contentHeight = screenHeight - 60 - 60;
     document.getElementById("content").style.height = contentHeight + "px";
-}
+});
+
+
+// 是否开始监听标记
+var start = false;
+// 实时下载、上传速度
+var downloadNow = 0;
+var uploadNow = 0;
 
 
 /*
@@ -19,33 +26,13 @@ function selectDevice() {
             "device_index": $("#device-index").val()
         },
         success: function (data) {
-
+            start = true;
         },
         error: function () {
             alert('Error: selectDevice')
         }
     })
 }
-
-
-// function test() {
-//     $.ajax({
-//         url: "/flow/burst/",
-//         type: "GET",
-//         success: function (data) {
-//             let data_obj = JSON.parse(data);
-//             {#$("#result1").text(data_obj["upload"])# }
-//             {#$("#result2").text(data_obj["download"])# }
-//             {#$("#result3").text(data_obj["upload_now"])# }
-//             {#$("#result4").text(data_obj["download_now"])# }
-//             {#$("#result5").text(data_obj["upload_history"])# }
-//             {#$("#result6").text(data_obj["download_history"])# }
-//         },
-//         error: function () {
-//             alert('Error.')
-//         }
-//     })
-// }
 
 
 /*
@@ -58,12 +45,121 @@ function getFlowData() {
         data: {
         },
         success: function (data) {
+            var results = JSON.parse(data);
 
+            $("#real-time-download").text(results["download_now"]);
+            $("#real-time-upload").text(results["upload_now"]);
+            $("#history-download").text(results["download_history"]);
+            $("#history-upload").text(results["upload_history"]);
+
+            downloadNow = results["download_now"];
+            uploadNow = results["upload_now"];
         },
         error: function () {
             alert('Error: getFlowData')
         }
     })
+}
+
+
+/*
+* 地点分析
+*/
+function analyzeAddresses() {
+    $.ajax({
+        url: "address/",
+        type: "GET",
+        data: {
+        },
+        success: function (data) {
+            var results = JSON.parse(data);
+            var topDownloadAddresses = sortDict(results["download"]);
+            var topUploadAddresses = sortDict(results["upload"]);
+
+            $("#top-download-addresses").text(topDownloadAddresses);
+            $("#top-upload-addresses").text(topUploadAddresses);
+        },
+        error: function () {
+            alert('Error: analyzeAddresses')
+        }
+    })
+}
+
+
+/*
+* 域名分析
+*/
+function analyzeNames() {
+    $.ajax({
+        url: "name/",
+        type: "GET",
+        data: {
+        },
+        success: function (data) {
+            var results = JSON.parse(data);
+            var topDownloadNames = sortDict(results["download"]);
+            var topUploadNames = sortDict(results["upload"]);
+
+            $("#top-download-names").text(topDownloadNames);
+            $("#top-upload-names").text(topUploadNames);
+        },
+        error: function () {
+            alert('Error: analyzeNames')
+        }
+    })
+}
+
+
+/*
+* 突发流量分析
+*/
+function analyzeBurst() {
+    $.ajax({
+        url: "burst/",
+        type: "GET",
+        data: {
+        },
+        success: function (data) {
+            alert(data)
+            var results = JSON.parse(data);
+            var topBurst = sortDict(results["burst"]);
+
+            $("#top-burst").text(topBurst);
+        },
+        error: function () {
+            alert('Error: analyzeBurst')
+        }
+    })
+}
+
+
+/*
+* 根据字典的值对键进行排序
+*/
+function sortDict(dict) {
+    var top = 5;
+    if (top > dict.length) {
+        top = dic.length;
+    }
+
+    var sortedKeys = [];
+    
+    for (var i = 0; i < top; i++) {
+        var maxKey;
+        var max = 0;
+
+        for (var key in dict) {
+            if (dict[key] > max) {
+                maxKey = key;
+                max = dict[key];
+            }
+        }
+
+        sortedKeys.push(maxKey);
+        delete dict[maxKey];
+    }
+
+    return sortedKeys;
 }
 
 
@@ -82,6 +178,7 @@ function plotRealTimeTrafficChart() {
 
     // 添加折线图数据
     function addData(shift) {
+        // 计算此刻时间
         now = new Date();
         now = [now.getHours(), now.getMinutes(), now.getSeconds()];
         for (var i = 0; i < 3; i++) {
@@ -90,11 +187,22 @@ function plotRealTimeTrafficChart() {
             }
         }
         now = now.join(":")
+        
+        // 从后端更新流量数据
+        getFlowData();
+        // 从后端更新地址分析结果
+        analyzeAddresses();
+        // 从后端更新域名分析结果
+        analyzeNames();
+        // 从后端更新突发流量分析结果
+        // analyzeBurst();
 
+        // 列表加入新数据
         time.push(now);
-        download.push((Math.random()) * 10);
-        upload.push((Math.random()) * 10);
+        download.push(downloadNow)
+        upload.push(uploadNow)
 
+        // 列表平移
         if (shift) {
             time.shift();
             download.shift();
@@ -139,7 +247,9 @@ function plotRealTimeTrafficChart() {
 
     // 设置更新时间间隔
     setInterval(function () {
-        addData(time.length > 100);
+        if (start) {
+            addData(time.length > 100);
+        }
 
         trafficChart.setOption({
             xAxis: {
