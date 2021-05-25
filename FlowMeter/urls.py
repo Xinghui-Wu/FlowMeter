31de8759825ab7dcffd71f68ef9dcb95ee9cc4f0
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-05-25 12:58:55 +0800

    Change the URL of the index page.

--- FlowMeter/urls.py
+++ FlowMeter/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('flow/', include('flow.urls'))
+    path('', include('flow.urls'))
 ]
